7b5dfc7f9063689f6251bc3d9d2dd74946bd5362
AngelMoraDev <angelmomaa@gmail.com>
2021-08-26 13:23:25 -0500

    create property model with rspec test

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Property, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  # expect name to be present and validates :name, length: { minimum: 1, maximum: 128 }
+  
 end
