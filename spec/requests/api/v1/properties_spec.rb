7842f67618a8a0b58c6ab5e28ad6dd538d311a65
AngelMoraDev <angelmomaa@gmail.com>
2021-08-27 14:21:41 -0500

    change ruby setup action source

--- spec/requests/api/v1/properties_spec.rb
+++ spec/requests/api/v1/properties_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Properties', type: :request do
   let(:random_property) { build(:random_property) }
+  let(:new_property) do
+    {
+      name: 'A new property!',
+      type_of_property: 'house',
+      street: 'Mary Street',
+      external_number: '12 a',
+      internal_number: '1b',
+      neighborhood: 'Condesa',
+      city: 'Mexico City',
+      country: 'MX',
+      rooms: 4,
+      bathrooms: 3,
+      comments: 'Includes forniture!'
+    }
+  end
 
   describe 'GET /index' do
     it 'returns http success' do
@@ -12,7 +27,7 @@ RSpec.describe 'Api::V1::Properties', type: :request do
 
   describe 'POST /properties/:id' do
     it 'returns http success' do
-      post "/api/v1/properties/#{random_property.id}"
+      post "/api/v1/properties/"
       expect(response).to have_http_status(:success)
     end
   end
